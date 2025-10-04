8db8fe26bddb837b6b2d4e9eb258920328d8c13f
Mr.Yoso <jeffrickaquino011@gmail.com>
2025-10-04 19:53:24 +0800

    Shaman.java

--- Shaman.java
+++ Shaman.java
@@ -10,6 +10,6 @@ public class Shaman extends Character
     health += 5;
     stamina += 5;
     mana += 5;
-    System.out.println("Player all stats increased by 5!");
+    System.out.println("Player used Heal! All stats increased by 5!");
     }
 }
